e633c7862e078b2bc95c87b1eab6474baf210ac3
Shaun Haage <shaage@digitalriver.com>
2017-01-31 04:41:29 +0200

    commit to master

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var useref = require('gulp-useref');
+var cssnano = require('gulp-cssnano');
+var runSequence = require('run-sequence');
+
+
+gulp.task('sass', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('scss/**/*.scss', ['sass']); 
+  // Other watchers
+});
+
+gulp.task('useref', function() {
+  return gulp.src('*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function (callback) {
+  runSequence( 
+    ['sass','useref'],
+    callback
+  )
+})
+
